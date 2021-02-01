9d22f7096d3e64f80cb31b51ca8a620ac4cddc6f
Alex <alexandre.barczyk@ig2i.centralelille.fr>
2021-02-01 13:56:35 +0100

    initial commit with files

---
+++ src/main/java/ig2i/geocache/GeocacheApplication.java
@@ -0,0 +1,13 @@
+package ig2i.geocache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeocacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ig2i/geocache/GeocacheApplicationTests.java
@@ -0,0 +1,14 @@
+package ig2i.geocache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
